40be46ee5397b3e0c7b0b82510149859b11d6883
HindsD <dhinds423@gmail.com>
2024-12-10 09:59:18 -0600

    Nav fixes

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// doin stuff
+const isGithubPages = window.location.hostname === "hindsd.github.io";
+if (isGithubPages) {
+    document.querySelectorAll("a").forEach(link => {
+        link.href = "/DannyZachRaudPogchamp/" + link.getAttribute("href");
+    });
+}
